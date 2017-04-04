0b55800162f30d7e8b5e15e9ae5a1d15c2cbfbf3
Sarah Schütz <sarahschuetz@gmx.at>
2017-04-04 23:56:05 +0200

    add logo

---
+++ resources/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+    <nuxt-link to="/">
+        <img src="/img/logo.png">
+    </nuxt-link>
+</template>
+
+// --------------------------------------------------
+
+<script>
+
+</script>
+
+// --------------------------------------------------
+
+<style lang="scss" scoped>
+
+    img {
+        position: absolute;
+        width: 70px;
+        top: 20px;
+        right: 15px;
+    }
+
+</style>
