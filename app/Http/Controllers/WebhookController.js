16f268f7e554915127fed7e48b725b6d0eae6822
Sarah Sauseng <sarahsauseng@Sarahs-MacBook-Pro.local>
2017-04-18 15:30:17 +0200

    Testing differentiating functions..

--- app/Http/Controllers/WebhookController.js
+++ app/Http/Controllers/WebhookController.js
@@ -22,6 +22,21 @@ class WebhookController {
         })
     }
 
+    * get_name (request, response) {
+        let params = request.all()
+        let query = params['result']['resolvedQuery']
+        let catName = params['originalRequest']['data']['cat']['name']
+        let middleware = request.middleware
+
+        let speech = 'Get_name_call: I am ' + catName + query + middleware
+
+        response.json({
+            speech: speech,
+            source: "purr-purr-purr-app",
+            displayText: speech
+        })
+    }
+
 }
 
 module.exports = new WebhookController()
--- app/Http/Middleware/Webhook.js
+++ app/Http/Middleware/Webhook.js
@@ -1,6 +1,10 @@
 'use strict'
 
 let WebhookController = use('App/Http/Controllers/WebhookController.js')
+let constants = [{
+    'name_const': ['name'],
+    'room_const': ['room']
+}]
 
 class Webhook {
 
@@ -8,6 +12,12 @@ class Webhook {
         let params = request.all()
         let query = params['result']['resolvedQuery']
         request.middleware = 'middleware call' + query
+
+        if(query.includes(constants.name_const[0])){
+            console.log('should call get_name')
+            yield WebhookController.get_name(request,response)
+        }
+
         yield WebhookController.webhook(request,response)
     }
 
