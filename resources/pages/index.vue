8b0ac6820c243b1ab9cddf373f27054f0002b9f6
Sarah Schütz <sarahschuetz@gmx.at>
2017-04-19 16:33:56 +0200

    ADD text and make responsive

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -5,8 +5,8 @@
             <h1 class="center-text">purr, purr, purr!</h1>
 
             <p class="center-text">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br>
-                Aenean commodo ligula eget dolor. Aenean massa.</p>
+                You love cats? Who doesn't!<br>
+                Nevertheless they sometimes make us crazy.</p>
             
             <nuxt-link to="/play">
                 <button class="btn border-pink center-block">miaow</button>
@@ -38,16 +38,16 @@
 
 <style lang="scss" scoped>
 
+    @import '~assets/scss/mixins';
+
     .background { 
         width: 100vw;
         height: 100vh;
 
         .introduction {
 
-            $width: 425;
             $height: 200;
 
-            width: #{$width}px;
             height: #{$height}px;
             position: relative;
             top: calc(45% - #{$height / 2}px);
@@ -55,13 +55,16 @@
 
             h1 {
                 font-size: 42px;
+                @include responsive-property("font-size", 30px, 42px);
                 margin: 0;
+                @include responsive-property("margin-top", 12px, 0);
                 margin-bottom: 10px;
             }
 
             p {
                 margin-top: 0;
                 margin-bottom: 17px;
+                @include responsive-property("font-size", 13px, 16px);
             }
         }
 
