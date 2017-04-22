09e553e77d6036148e7786edc4c4882e457b72de
Sarah Schütz <sarahschuetz@gmx.at>
2017-04-22 16:10:36 +0200

    ADD responsiveness contact

--- resources/pages/contact.vue
+++ resources/pages/contact.vue
@@ -7,6 +7,7 @@
                 <div class="small-width">
                     <p class="center-text column-2">
                         <span>Sarah Sauseng</span><br>
+                        sarah.sauseng@me.com<br>
                         FH Hagenberg
                     </p>
                     <p class="center-text column-2">
@@ -41,52 +42,64 @@
 
 <style lang="scss" scoped>
 
-    p {
-        margin-bottom: 50px;
+    @import '~assets/scss/mixins';
 
-        &.column-2 {
-            width: 50%;
+    section {
+        padding-bottom: 250px;
+
+        p {
+            margin-bottom: 50px;
+
+            &.column-2 {
+                @include responsive-property('width', 100%, 186px, 50%);
+                float: left;
+            }
+
+            span {
+                font-weight: 700;
+            }
+        }
+
+        .text-input {
+            clear: both;
+            height: 45px;
+            margin: 0 auto 25px;
+            @include responsive-property('padding-right', 0, 0, 0, 100px);
+            @include responsive-property('width', 100%, 100%, 600px, 100%);
+
+            input, textarea {
+                @include responsive-property('width', 100%, 100%, 400px, calc(100% - 200px));
+                float: right;
+                @include responsive-property('margin-right', 0, 0, 100px);
+            }
+        }
+
+        label, textarea, input {
             float: left;
         }
 
-        span {
-            font-weight: 700;
+        label {
+            width: 100px;
+            @include responsive-property('margin-top', 15px, 15px, 9px);
+            @include responsive-property('margin-bottom', 5px, 5px, 0);
         }
-    }
 
-    .text-input {
-        clear: left;
-        height: 45px;
-        margin-bottom: 25px;
-        padding-right: 100px;
-
-        input, textarea {
-            width: calc(100% - 100px);
+        button {
+            @include responsive-property('width', 100%, 100%, 400px, calc(100% - 200px));
+            clear: both;
+            display: block;
+            position: relative;
+            top: 25px;
+            margin: 0 auto;
         }
-    }
 
-    label, textarea, input {
-        float: left;
-    }
+        .small-width {
+            @include responsive-property('width', 100%, 450px);
+            margin: 0 auto;
 
-    label, button {
-        width: 100px;
-    }
-    
-    button {
-        width: calc(100% - 200px);
-        margin-left: 100px;
-        clear: left;
-        margin-top: 25px;
-        margin-bottom: 250px;
-    }
-
-    .small-width {
-        width: 450px;
-        margin: 0 auto;
-
-        p {
-            margin-top: 0;
+            p {
+                margin-top: 0;
+            }
         }
     }
 
